ed9bef36f5ea118a42b01a5f368e994c421601fb
Ariyan Khan <ariyankhan46@gmail.com>
2017-07-21 04:08:21 +0530

    Added document tree manipulation functionality

--- dist/vector.svg.js
+++ dist/vector.svg.js
@@ -8,7 +8,7 @@
  *
  * Codebase: https://github.com/ariyankhan/vector.svg
  * Homepage: https://github.com/ariyankhan/vector.svg#readme
- * Date: Thu Jul 20 2017 19:20:35 GMT+0530 (IST)
+ * Date: Fri Jul 21 2017 04:08:03 GMT+0530 (IST)
  */
 
 (function(root, factory) {
@@ -949,18 +949,6 @@
             }
         },
 
-        byId: function() {
-
-        },
-
-        query: function() {
-
-        },
-
-        queryAll: function() {
-
-        },
-
         /**
          * Returns underlying DOM element.
          * Remember after creating a wrapper,
@@ -1698,6 +1686,126 @@
                 return null;
         }
     };
+
+    // Addition of the Document Tree manipulation functionality
+    Vector.merge(Element.prototype, {
+
+        /**
+         * Returns all child SVGElement as Array of wrappers
+         * @returns {Array}
+         */
+        children: function() {
+            var dom = this._domElement,
+                outs = [],
+                i = 0,
+                length,
+                node,
+                nodes;
+            nodes = dom.childNodes;
+            length = nodes.length;
+            for (; i < length; ++i) {
+                node = nodes.item(i);
+                if (node instanceof window.SVGElement)
+                    outs.push(Vector.wrap(node));
+            }
+            return outs;
+        },
+
+        insert: function(elem, index) {
+            if (!(elem instanceof Element))
+                return this;
+            if (isNullOrUndefined(index)) {
+                return this.append(elem);
+            }
+
+            var children = this.children();
+            if (children.length === 0) {
+                return this.append(elem);
+            }
+
+            index = +index;
+            index = isNaN(index) ? 0 : index;
+            index = max(0, index);
+            index = floor(index);
+
+            if (index >= children.length) {
+                return this.append(elem);
+            }
+
+            this.node().insertBefore(elem.node(), children[index].node());
+            return this;
+        },
+
+        append: function(elem) {
+            if (!(elem instanceof Element))
+                return this;
+            this.node().appendChild(elem.node());
+            return this;
+        },
+
+        /**
+         * Here if index is out of bounds then nothing will happen
+         *
+         * @param elem can be any wrapper element or index of element
+         * @returns {*}
+         */
+        remove: function(elem) {
+            if (elem instanceof Element) {
+                this.node().removeChild(elem.node());
+            } else if (typeof elem === "number") {
+                return this.remove(this.children()[elem]);
+            }
+            return this;
+        },
+
+        has: function(elem) {
+            if (!(elem instanceof Element))
+                return false;
+            var children = this.children(),
+                length = children.length,
+                i = 0;
+            for (; i < length; ++i) {
+                if (children[i].node() === elem.node())
+                    return true;
+            }
+            return false;
+        },
+
+        replace: function(newElem, oldElem) {
+            if (!(newElem instanceof Element) || !(oldElem instanceof Element))
+                return this;
+
+            this.node().replaceChild(newElem.node(), oldElem.node());
+            return this;
+        },
+
+        /**
+         * This method sets text content of any node.
+         * It is useful for <title> or <desc> elements
+         * @param text
+         * @returns {*}
+         */
+        textContent: function(text) {
+            if (text === undefined)
+                return this.node().textContent;
+            else {
+                this.node().textContent = text;
+                return this;
+            }
+        },
+
+        byId: function() {
+
+        },
+
+        query: function() {
+
+        },
+
+        queryAll: function() {
+
+        }
+    });
 
     /**
      * Wrapper for SVGGraphicsElement native interface
